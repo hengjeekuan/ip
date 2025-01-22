0bb16f789de40964fdfd0dfd317f91cd059011e5
Jee <hengjeekuan@gmail.com>
2025-01-23 01:59:14 +0800

    Added Echo function

--- src/main/java/Jeenius.java
+++ src/main/java/Jeenius.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Jeenius {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("----------------------------------------");
         System.out.println("Hello! I'm Jeenius");
         System.out.println("What can I do for you today?");
         System.out.println("----------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("----------------------------------------");
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("----------------------------------------");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("----------------------------------------");
+                break;
+            }
+
+            System.out.println("----------------------------------------");
+            System.out.println(userInput);
+            System.out.println("----------------------------------------");
+        }
+        scanner.close();
     }
 }

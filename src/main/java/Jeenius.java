90f184a573329a5f4e8acdfb4b416a0ae5fa3dc9
Jee <hengjeekuan@gmail.com>
2025-01-23 15:09:17 +0800

    Added ability to store text and list them

--- src/main/java/Jeenius.java
+++ src/main/java/Jeenius.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Jeenius {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<String> storage = new ArrayList<>();
 
         System.out.println("----------------------------------------");
         System.out.println("Hello! I'm Jeenius");
@@ -17,7 +20,19 @@ public class Jeenius {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("----------------------------------------");
                 break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+                for (int x = 0; x < storage.size(); x = x + 1) {
+                    int num = x + 1;
+                    String input = storage.get(x);
+                    System.out.println(num + ". " + input);
+                }
+            } else {
+                storage.add(userInput);
+                System.out.println("added:" + userInput);
             }
+
+
+
 
             System.out.println("----------------------------------------");
             System.out.println(userInput);

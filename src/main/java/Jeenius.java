e2e37e20ad035da4013187f7408afd71614ad125
Jee <hengjeekuan@gmail.com>
2025-01-23 15:38:53 +0800

    Added ability to mark tasks as done

--- src/main/java/Jeenius.java
+++ src/main/java/Jeenius.java
@@ -2,10 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class Jeenius {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<String> storage = new ArrayList<>();
+        List<Task> storage = new ArrayList<>();
 
         System.out.println("----------------------------------------");
         System.out.println("Hello! I'm Jeenius");
@@ -23,11 +24,23 @@ public class Jeenius {
             } else if (userInput.equalsIgnoreCase("list")) {
                 for (int x = 0; x < storage.size(); x = x + 1) {
                     int num = x + 1;
-                    String input = storage.get(x);
-                    System.out.println(num + ". " + input);
+                    Task input = storage.get(x);
+                    String check = input.getStatusIcon();
+                    System.out.println(num + ".[" + check + "] "+ input.getDescription());
                 }
+            } else if (userInput.startsWith("mark")){
+                String[] parts = userInput.split(" ");
+                int taskNumber = Integer.parseInt(parts[1]) - 1;
+                Task task = storage.get(taskNumber);
+                task.mark();
+            } else if (userInput.startsWith("unmark")){
+                String[] parts = userInput.split(" ");
+                int taskNumber = Integer.parseInt(parts[1]) - 1;
+                Task task = storage.get(taskNumber);
+                task.unmark();
             } else {
-                storage.add(userInput);
+                Task nextInput = new Task(userInput);
+                storage.add(nextInput);
                 System.out.println("added:" + userInput);
             }
 

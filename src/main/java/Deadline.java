72228e136532ee66ad3bb6989cb2b546570c733c
Jee <hengjeekuan@gmail.com>
2025-02-06 00:28:03 +0800

    Update event and deadline task

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,19 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    private LocalDateTime by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+        this.by = LocalDateTime.parse(by, formatter);
     }
 
-    public String getBy() { return this.by;}
+    public String getBy() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mm a");
+        return by.format(formatter);
+    }
 
     @Override
     public String toString() {

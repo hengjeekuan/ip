c1ac68a0401b5e4293f954f1e7769ff943bdf6c1
Jee <hengjeekuan@gmail.com>
2025-02-11 19:49:54 +0800

    Add JavaDoc comments to Task.java

--- src/main/java/jeenius/task/Task.java
+++ src/main/java/jeenius/task/Task.java
@@ -1,9 +1,19 @@
 package jeenius.task;
 
+/**
+ * Represents a generic task with a description and completion status.
+ * Serves as parent class for specific task types (Todo, Deadline, Event).
+ */
 public abstract class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Creates a task with input description.
+     * Task is initially marked as not done.
+     *
+     * @param description Textual description of the task
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
@@ -33,5 +43,12 @@ public abstract class Task {
         return "[" + this.getStatusIcon() + "] "+ this.getDescription();
     }
 
+    /**
+     * Converts the task to a standardized format for file storage.
+     * This format should be implemented by subclasses to ensure consistency
+     * in saving and loading tasks from files.
+     *
+     * @return A formatted string representing the task, suitable for file storage
+     */
     public abstract String toFileFormat();
 }

80d9aa3952b5e4c088aba52f941955d7d9f8cccc
Jee <hengjeekuan@gmail.com>
2025-02-18 18:40:22 +0800

    Add FXML

---
+++ src/main/java/jeenius/MainWindow.java
@@ -0,0 +1,52 @@
+package jeenius;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+/**
+ * Controller for the main GUI.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Jeenius jeenius;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/IMG_0134.JPG"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/IMG_2354.JPG"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    /** Injects the Jeenius instance */
+    public void setDuke(Jeenius j) {
+        jeenius = j;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = jeenius.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getJeeniusDialog(response, dukeImage)
+        );
+        userInput.clear();
+    }
+}
